c34dc8bc1f6d65ba07ab3971784a0486a92889eb
Itlink75 <58917844+Itlink75@users.noreply.github.com>
2020-09-07 16:08:50 +0200

    Update pyrebase.py

--- pyrebase/pyrebase.py
+++ pyrebase/pyrebase.py
@@ -445,7 +445,7 @@ def raise_detailed_error(request_object):
     except HTTPError as e:
         # raise detailed error message
         # TODO: Check if we get a { "error" : "Permission denied." } and handle automatically
-        raise HTTPError(e, request_object.text)
+        print(request_object.text)
 
 
 def convert_to_pyre(items):
